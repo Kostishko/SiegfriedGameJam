7ec471d896b11157957f84435630a6c8bb07c9a2
fortey <fortey@inbox.ru>
2021-11-03 12:32:35 +0700

    added hero animator

--- Siegfried/Assets/Scripts/Characters/Mover.cs
+++ Siegfried/Assets/Scripts/Characters/Mover.cs
@@ -8,15 +8,30 @@ public class Mover : MonoBehaviour
 
     private Rigidbody2D _rb;
     private Vector2 movement;
+    private Animator _animator;
+    private readonly int animHorizontal = Animator.StringToHash("Horizontal");
+    private readonly int animVertical = Animator.StringToHash("Vertical");
+    private readonly int animSpeed = Animator.StringToHash("Speed");
+
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        UpdateAnimatorParameters();
+    }
+
+    private void UpdateAnimatorParameters()
+    {
+        _animator.SetFloat(animHorizontal, movement.x);
+        _animator.SetFloat(animVertical, movement.y);
+        _animator.SetFloat(animSpeed, movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
