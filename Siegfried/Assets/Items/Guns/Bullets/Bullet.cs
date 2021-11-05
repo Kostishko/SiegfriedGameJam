42e643370a3673938b9f492ad30ff7b5c4102bfc
fortey <fortey@inbox.ru>
2021-11-05 14:02:34 +0700

    MachineGun

---
+++ Siegfried/Assets/Items/Guns/Bullets/Bullet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _speed = 100f;
+
+    public void Setup(Vector2 direction)
+    {
+        GetComponent<Rigidbody2D>().AddForce(direction * _speed, ForceMode2D.Impulse);
+        Destroy(gameObject, 5f);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Enemy target = other.GetComponent<Enemy>();
+        if (target != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
