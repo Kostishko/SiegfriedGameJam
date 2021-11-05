2dab586416fe57dad88285735ba46ea8a9432e9f
fortey <fortey@inbox.ru>
2021-11-05 18:29:58 +0700

    melee attack

---
+++ Siegfried/Assets/Items/Sword/Plume.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plume : MonoBehaviour
+{
+    public Action<Collider2D> OnEnter;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        OnEnter?.Invoke(other);
+    }
+
+    private void Start()
+    {
+        Destroy(GetComponent<PolygonCollider2D>(), 0.1f);
+    }
+}
