f889d4d9d30e2feeab2ec014313c2e1006005e83
fortey <fortey@inbox.ru>
2021-11-06 15:14:47 +0700

    inventory

---
+++ Siegfried/Assets/Items/ItemsScripts/ManaPotion.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Mana potion", menuName = "ScriptableObjects/Items/Mana Potion", order = 1)]
+public class ManaPotion : Item
+{
+    public int amount;
+    public override void Use()
+    {
+        FindObjectOfType<CharacterState>().TakeMana(amount);
+    }
+}
