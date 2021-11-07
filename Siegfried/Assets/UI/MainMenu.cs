f848dec94a920cf9a90fb43605811618671921a7
Diana <72194867+dianasun97@users.noreply.github.com>
2021-11-07 17:07:39 +0200

    Fix game start

--- Siegfried/Assets/UI/MainMenu.cs
+++ Siegfried/Assets/UI/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void StartGame() {
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("Africa");
     }
     public void QuitGame() {
         Debug.Log("Quit!");
